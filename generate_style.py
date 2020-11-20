a1ccc5e752071d3da468191df15fccaf724adff8
Magalie Brossard <magalie.brossard@grand-chatellerault.fr>
2020-11-20 15:14:52 +0100

    [CAPC] Test modif fond de plan - Tests building

--- generate_style.py
+++ generate_style.py
@@ -794,6 +794,9 @@ style = {
    },
    'building_clr': '"#bbbbbb"',
    'building_ol_clr': '"#333333"',
+   #'building_clr': '#d3d0c7',
+   #'building_ol_clr': '#d3d0c7',
+
    'building_ol_width': {
       0:0,
       16:0.1,
@@ -1248,7 +1251,7 @@ namedstyles = {
       #'canal_clr': '153 179 204',
       'canal_clr': '170 216 249',
 
-      'building_clr': '#d3d0c7',
+      #'building_clr': '#d3d0c7',
       #'building_ol_clr': '#d3d0c7',
 
       'motorway_ol_clr': '186 110 39',
