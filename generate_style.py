6663c3692570a9e9c1c25236aa88475e35dfd3bb
Magalie Brossard <magalie.brossard@grand-chatellerault.fr>
2020-11-23 11:49:20 +0100

    [CAPC] Test - Changement couleur buildings

--- generate_style.py
+++ generate_style.py
@@ -792,8 +792,10 @@ style = {
       0: 0,
       15:1
    },
-   'building_clr': '"#bbbbbb"',
-   'building_ol_clr': '"#333333"',
+   #'building_clr': '"#bbbbbb"',
+   #'building_ol_clr': '"#333333"',
+   'building_clr': '#d3d0c7',
+   'building_ol_clr': '#d3d0c7',
 
    'building_ol_width': {
       0:0,
